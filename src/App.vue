14f6c154e76628adcd94b3ab55bbb784de19e332
Saltypeesh <tungthanh254@gmail.com>
2025-04-25 11:28:23 +0700

    Add vue-router for navigation and refactor components to use RouterLink. Updated package.json and package-lock.json to include vue-router dependency. Modified App.vue to use RouterView for routing. Updated Navbar, HomeCards, JobListing, and JobListings components for routing links.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
-import Hero from '@/components/Hero.vue'
-import HomeCards from '@/components/HomeCards.vue'
-import JobListings from '@/components/JobListings.vue'
-import jobData from '@/jobs.json'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <Navbar />
-  <Hero />
-  <HomeCards />
-  <JobListings :limit="3" :showButton="true" />
+  <RouterView />
 </template>
