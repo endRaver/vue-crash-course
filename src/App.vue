a6d84b31cb3c316841360cb0916a8939126d7aa4
Saltypeesh <tungthanh254@gmail.com>
2025-04-25 11:01:40 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
+import jobData from '@/jobs.json'
+</script>
+
+<template>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
+</template>
